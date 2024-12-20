c46e0c30042eb9c9183154a3b149ae68f4fd40fe
Olivier Liu <liuolivier93@gmail.com>
2024-12-20 14:08:34 +0100

    création du script python pour le modèle de scoring

---
+++ Untitled2.py
@@ -0,0 +1,109 @@
+# %% [markdown]
+# # Projet 7 : Implémentez un modèle de scoring
+
+# %% [markdown]
+# Suite du projet 7. Implémentation du modèle de scoring via API
+
+# %% [markdown]
+# ## Import des librairies
+
+# %%
+# File system manangement
+import os
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# sklearn preprocessing for dealing with categorical variables
+from sklearn.preprocessing import LabelEncoder
+
+# Suppress warnings 
+import warnings
+warnings.filterwarnings('ignore')
+
+# Affichage en ligne des graphiques
+# %matplotlib inline
+
+# Options pour améliorer l'affichage des données
+pd.set_option('display.max_columns', 50)
+# pd.set_option('display.max_rows', 100)
+sns.set(style="whitegrid")
+
+# %% [markdown]
+# ## Chargement des fichiers Pickle et des Clients
+
+# %%
+import pickle
+
+# Vérifier que le fichier pickle existe et n'est pas vide
+pickle_file_path = 'best_lightgbm_model.pkl'
+if os.path.exists(pickle_file_path):
+    file_size = os.path.getsize(pickle_file_path)
+    if file_size == 0:
+        print("Le fichier pickle est vide.")
+    else:
+        print(f"Le fichier pickle a une taille de {file_size} octets.")
+else:
+    print("Le fichier pickle n'existe pas.")
+
+# Charger le modèle LightGBM depuis le fichier pickle
+with open('best_lightgbm_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# Vérifiez que le modèle est chargé
+# print(model)
+
+# Charger les données des clients (X) à partir du CSV
+client_data = pd.read_csv('client_data.csv')
+# print(client_data.sample())
+
+# %%
+client_data.num__SK_ID_CURR.sample(10)
+
+# %% [markdown]
+# ## Flask API
+
+# %%
+from flask import Flask, jsonify, request
+import random
+
+# Créer l'application Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    random_client_id = random.choice(client_data['num__SK_ID_CURR'])
+    return f"Bienvenue sur la page d'accueil! Un client ID : {random_client_id}"
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    # Récupérer l'ID du client passé en paramètre de la requête
+    client_id = request.args.get('id', type=int)
+
+    # Vérifier si l'ID du client existe dans les données
+    if client_id not in client_data['num__SK_ID_CURR'].values:
+        return jsonify({'error': f'Client ID {client_id} non trouvé'}), 404
+    
+    # Sélectionner les données du client
+    client_info = client_data[client_data['num__SK_ID_CURR'] == client_id]
+
+    # Effectuer la prédiction
+    prediction = model.predict(client_info)
+
+    # Effectuer la probabilité de classe
+    prediction_proba = model.predict_proba(client_info)
+
+    # Retourner la prédiction sous forme de JSON
+    return jsonify({
+        'client_id': client_id,
+        'prediction': int(prediction[0]),
+        'prediction_proba': float(prediction_proba[0][1])
+    })
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True, use_reloader=False)
+
+
